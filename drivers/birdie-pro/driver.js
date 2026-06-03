c4bc2fc326f7b27cac23f96c7778623ef090e868
Abe Haverkamp <abe.haverkamp@gmail.com>
2026-06-03 11:41:30 +0200

    v0.1.1 - Fixed Flow card listener registration

--- drivers/birdie-pro/driver.js
+++ drivers/birdie-pro/driver.js
@@ -7,6 +7,7 @@ module.exports = class MyDriver extends BirdieDriver {
    * onInit is called when the driver is initialized.
    */
   async onInit() {
+    await super.onInit();
     this.log('Birdie Pro Driver has been initialized');
   }
 };
